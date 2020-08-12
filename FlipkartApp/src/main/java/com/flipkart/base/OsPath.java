6f64192872fb9fb9815467997fff03ab33f1a44a
santosh DC <santosh DC@Santosh>
2020-08-12 21:09:46 +0530

    Commit

---
+++ FlipkartApp/src/main/java/com/flipkart/base/OsPath.java
@@ -0,0 +1,46 @@
+package com.flipkart.base;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class OsPath {
+	
+	public static Logger log=LogManager.getLogger(OsPath.class.getName());
+	//Provide browser exe path based on OS
+	public static String getPath(String browser) 
+	{
+		log.info("Browser to initialize="+browser);
+		String OS=System.getProperty("os.name");
+		String driverPath=null;
+		if(OS.toUpperCase().contains("WINDOWS"))
+		{
+			log.info("Operating system found : WINDOWS");
+			if(browser.toUpperCase().contains("CHROME"))
+			{
+				driverPath=".\\src\\main\\java\\com\\flipkart\\resources\\Chrome\\chromedriver.exe";
+				
+				
+			}
+			else if(browser.toUpperCase().contains("FF")||browser.toUpperCase().contains("FIRE"))
+			{
+				driverPath=".\\src\\main\\java\\com\\flipkart\\resources\\geckodriver.exe";
+				
+			}
+		}
+		else if(OS.toUpperCase().contains("MAC"))
+		{
+			log.info("Operating System found : MAC");
+			if(browser.toUpperCase().contains("CHROME"))
+			{
+				driverPath=".\\src\\main\\java\\com\\flipkart\\resources\\chromedriver.exe";
+			}
+			else if(browser.toUpperCase().contains("FF")||browser.toUpperCase().contains("FIRE"))
+			{
+				driverPath=".\\src\\main\\java\\com\\flipkart\\resources\\geckodriver.exe";
+			}
+		}
+		return driverPath;
+	}
+
+	
+}
